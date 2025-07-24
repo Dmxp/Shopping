397ae7246824780b0287592c30206d39aada6061
Dmxp <d.palaumov@yandex.ru>
2025-07-24 14:40:40 +0500

    4.2 Реализация списков через LinearLayout

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,30 +2,50 @@ package com.example.shoppinglist.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  viewModel: MainViewModel
-
-    private var count = 0
+    private lateinit var llShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            Log.d("mainActivityYest", it.toString())
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeenableState(item)
+            showList(it)
+
+        }
+    }
+    private fun showList(list: List<ShopItem>){
+        llShopList.removeAllViews()
+        for (shopItem in list ){
+            val layoutId = if (shopItem.enabled){
+                R.layout.item_shop_enabled
+            } else{
+                R.layout.item_shop_disabled
             }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeenableState(shopItem)
+                true
+            }
+            llShopList.addView(view)
         }
     }
 }
